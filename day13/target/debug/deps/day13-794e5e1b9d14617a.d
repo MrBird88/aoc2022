392e18bad1b419ad321b7cac48acb22080217185
MrBird88 <110942324+MrBird88@users.noreply.github.com>
2022-12-16 11:47:11 -0700

    need to learn how to git

--- day13/target/debug/deps/day13-794e5e1b9d14617a.d
+++
@@ -1,5 +0,0 @@
-c:\Users\roose\Code\Rust\aoc2022\day13\target\debug\deps\day13-794e5e1b9d14617a.rmeta: src\main.rs
-
-c:\Users\roose\Code\Rust\aoc2022\day13\target\debug\deps\day13-794e5e1b9d14617a.d: src\main.rs
-
-src\main.rs:
